9d0ec4c9248a749eef20d115b9934bf04951655b
Aralek13 <nolwenn.myran@free.fr>
2021-01-28 11:19:55 +0100

    added createUser and login route

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -4,8 +4,10 @@
 namespace App\Controller;
 
 
+use App\BL\UserManager;
 use App\Entity\User;
-use Doctrine\Persistence\ObjectManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
@@ -16,22 +18,27 @@ class UserController
      * @var UserPasswordEncoderInterface
      */
     private $encoder;
+    /**
+     * @var EntityManagerInterface
+     */
+    private $em;
 
     /**
      * AppFixtures constructor.
      * @param UserPasswordEncoderInterface $encoder
      */
-    public function __construct(UserPasswordEncoderInterface $encoder)
+    public function __construct(UserPasswordEncoderInterface $encoder, EntityManagerInterface $em)
     {
         $this->encoder = $encoder;
+        $this->em = $em;
     }
 
     /**
      * @Route("/register", name="createUser")
      * @param Request $request
-     * @param ObjectManager $manager
+     * @param UserManager $manager
      */
-    public function register(Request $request, ObjectManager $manager)
+    public function register(Request $request, UserManager $manager)
     {
         $data = json_decode($request->getContent(), true);
         $email = $data['email'];
@@ -42,7 +49,11 @@ class UserController
         $user->setPassword($password);
         $password = $this->encoder->encodePassword($user, $user->getPassword());
         $user->setPassword($password);
-        $manager->persist($user);
-        $manager->flush();
+        $this->em->persist($user);
+        $this->em->flush();
+
+        $data = array("username" => $user->getUsername(), "email" => $user->getEmail());
+
+        return new JsonResponse($data, 201);
     }
 }
