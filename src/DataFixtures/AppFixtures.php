e6780cb6c6f9cb870c7a43b2525562cd34098379
raphagoo <raphael.agostini@ynov.com>
2021-02-05 12:36:11 +0100

    better fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -96,55 +96,103 @@ class AppFixtures extends Fixture
             $manager->flush();
         }
 
-        $parkingExists = $manager->getRepository(Parking::class)->findOneBy(['name' => 'Parking Aix']);
-        if(!$parkingExists) {
-            $parking = new Parking();
-            $parking->setName('Parking Aix');
-            $parking->setPrice(30);
-            $manager->persist($parking);
-            $manager->flush();
-        }
-
-        $restaurantExists = $manager->getRepository(Restaurant::class)->findOneBy(['name' => 'Restaurant Aix']);
-        if(!$restaurantExists) {
-            $restaurant = new Restaurant();
-            $restaurant->setName('Restaurant Aix');
-            $restaurant->setPrice(30);
-            $manager->persist($restaurant);
-            $manager->flush();
-        }
-
-        $salleExists = $manager->getRepository(Salle::class)->findOneBy(['city' => 'Aix']);
-        if(!$salleExists) {
-            $salle = new Salle();
-            $salle->setName('Salle Aix');
-            $salle->setCity('Aix-en-Provence');
-            $salle->setAddress('123, qqpart');
-            $salle->setCapacity(300);
-            $salle->setPhoneNumber('0578986564');
-            if($parkingExists){
-                $salle->setParking($parkingExists);
-            }
-            else {
-                $salle->setParking($parking);
-            }
-            if($restaurantExists){
-                $salle->setRestaurant($restaurantExists);
-            }
-            else {
-                $salle->setRestaurant($restaurant);
-            }
-            $manager->persist($salle);
-            $manager->flush();
-        }
-
-
         $categoryExists = $manager->getRepository(Category::class)->findOneBy(['name' => 'Rock']);
         if(!$categoryExists) {
             //create category
             $category = new Category();
             $category->setName('Rock');
             $manager->persist($category);
+            $manager->flush();
+        }
+
+        $infoSalles = [['name' => 'Aix-en-Provence', 'capacity' => 150], ['name' => 'Bourges', 'capacity' => 100], ['name' => 'Cannes', 'capacity' => 200], ['name' => 'Dunkerque', 'capacity' => 100], ['name' => 'Echirolles', 'capacity' => 150]];
+
+        foreach ($infoSalles as $infoSalle) {
+            $parkingExists = $manager->getRepository(Parking::class)->findOneBy(['name' => 'Parking ' . $infoSalle['name']]);
+            if(!$parkingExists) {
+                $parking = new Parking();
+                $parking->setName('Parking ' . $infoSalle['name']);
+                $parking->setPrice(30);
+                $manager->persist($parking);
+                $manager->flush();
+            }
+
+            $restaurantExists = $manager->getRepository(Restaurant::class)->findOneBy(['name' => 'Restaurant ' . $infoSalle['name']]);
+            if(!$restaurantExists) {
+                $restaurant = new Restaurant();
+                $restaurant->setName('Restaurant ' . $infoSalle['name']);
+                $restaurant->setPrice(30);
+                $manager->persist($restaurant);
+                $manager->flush();
+            }
+
+            $salleExists = $manager->getRepository(Salle::class)->findOneBy(['city' => $infoSalle['name']]);
+            if (!$salleExists) {
+                $salle = new Salle();
+                $salle->setName('Salle ' . $infoSalle['name']);
+                $salle->setCity($infoSalle['name']);
+                $salle->setAddress('123, qqpart');
+                $salle->setCapacity($infoSalle['capacity']);
+                $salle->setPhoneNumber('0578986564');
+                if ($parkingExists) {
+                    $salle->setParking($parkingExists);
+                } else {
+                    $salle->setParking($parking);
+                }
+                if ($restaurantExists) {
+                    $salle->setRestaurant($restaurantExists);
+                } else {
+                    $salle->setRestaurant($restaurant);
+                }
+                $manager->persist($salle);
+                $manager->flush();
+            }
+
+            // create 20 events! Bam!
+            for ($i = 1; $i < 3; $i++) {
+                $event = new Event();
+                if($salleExists){
+                    $event->setSalle($salleExists);
+                }
+                else {
+                    $event->setSalle($salle);
+                }
+                $event->setName($infoSalle['name'] . ' - Event ' . $i);
+                $event->setArtistName('Artiste' . $i);
+                $event->setArtistDescription('Lorem Ipsum');
+                $event->setParking(true);
+                $event->setRestaurant(true);
+                if($categoryExists){
+                    $event->addCategory($categoryExists);
+                }
+                else {
+                    $event->addCategory($category);
+                }
+
+                $manager->persist($event);
+
+                for($j = 0; $j < 2; $j++) {
+                    $concert = new Concert();
+
+                    $dateTime = new DateTime('2021-02-08T20:00:00.000000Z');
+                    $dateTime = $dateTime->add(new \DateInterval('P' . $j . 'D'));
+
+                    $concert->setArtistDescription('Lorem Ipsum');
+                    $concert->setCategoryNumber(3);
+                    $concert->setDate($dateTime);
+                    $concert->setEvent($event);
+                    $concert->setOpeningTime($dateTime);
+                    $concert->setPercentage(5);
+                    $concert->setPriceMax(200);
+
+                    $dateTime = $dateTime->add(new \DateInterval('PT2H'));
+                    $concert->setTime($dateTime);
+
+                    $manager->persist($concert);
+
+                    $manager->getRepository(Concert::class)->createSeats($concert);
+                }
+            }
             $manager->flush();
         }
 
@@ -178,55 +226,6 @@ class AppFixtures extends Fixture
             $manager->persist($article);
         }
 
-        $manager->flush();
-
-        // create 20 events! Bam!
-        $nbExisting = count($manager->getRepository(Event::class)->findAll());
-
-        for ($i = $nbExisting; $i < $nbExisting + 20; $i++) {
-            $event = new Event();
-            if($salleExists){
-                $event->setSalle($salleExists);
-            }
-            else {
-                $event->setSalle($salle);
-            }
-            $event->setName('Event' . $i);
-            $event->setArtistName('Artiste' . $i);
-            $event->setArtistDescription('Lorem Ipsum');
-            $event->setParking(true);
-            $event->setRestaurant(true);
-            if($categoryExists){
-                $event->addCategory($categoryExists);
-            }
-            else {
-                $event->addCategory($category);
-            }
-
-            $manager->persist($event);
-
-            for($j = 0; $j < 2; $j++) {
-                $concert = new Concert();
-
-                $dateTime = new DateTime('2021-02-08T20:00:00.000000Z');
-                $dateTime = $dateTime->add(new \DateInterval('P' . $j . 'D'));
-
-                $concert->setArtistDescription('Lorem Ipsum');
-                $concert->setCategoryNumber(3);
-                $concert->setDate($dateTime);
-                $concert->setEvent($event);
-                $concert->setOpeningTime($dateTime);
-                $concert->setPercentage(5);
-                $concert->setPriceMax(200);
-
-                $dateTime = $dateTime->add(new \DateInterval('PT2H'));
-                $concert->setTime($dateTime);
-
-                $manager->persist($concert);
-
-                $manager->getRepository(Concert::class)->createSeats($concert);
-            }
-        }
         $manager->flush();
 
     }
